b10156b8be3ba1c733a43bc8a50d29eb168310e0
obscurya <prostunting@mail.ru>
2020-01-02 17:20:17 +0300

    Updates

---
+++ precache-manifest.e44b628dff70bdd04c42669456a567c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5debe448baacc865a4f6750f14c107d",
+    "url": "/map-app/index.html"
+  },
+  {
+    "revision": "fef1971fb5bda4325bda",
+    "url": "/map-app/static/css/2.7997c105.chunk.css"
+  },
+  {
+    "revision": "de7df8f566909cdaec15",
+    "url": "/map-app/static/css/main.7f1d6ead.chunk.css"
+  },
+  {
+    "revision": "fef1971fb5bda4325bda",
+    "url": "/map-app/static/js/2.7beba5a6.chunk.js"
+  },
+  {
+    "revision": "4302cec8d33c1a3a4473a3db31f00eca",
+    "url": "/map-app/static/js/2.7beba5a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de7df8f566909cdaec15",
+    "url": "/map-app/static/js/main.4663110c.chunk.js"
+  },
+  {
+    "revision": "d9c6658c9a603b667208",
+    "url": "/map-app/static/js/runtime-main.464878b9.js"
+  }
+]);
